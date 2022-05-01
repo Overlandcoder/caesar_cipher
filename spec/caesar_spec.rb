e6d53c2d16b285bc145a2809c9c78edd2b856c4e
Overlander <anuragnandal@hotmail.com>
2022-05-01 18:57:31 -0400

    Add tests for large positive and negative shifts

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -2,34 +2,38 @@ require './lib/caesar_cipher.rb'
 
 describe CaesarCipher do
   describe "encode" do
+    caesar = CaesarCipher.new
+
     it 'correctly encodes "z" with positive shift' do
-      caesar = CaesarCipher.new
       expect(caesar.encode("z", 5)).to eql("e")
     end
 
     it 'correctly encodes "Z" with positive shift' do
-      caesar = CaesarCipher.new
       expect(caesar.encode("Z", 5)).to eql("E")
     end
 
     it 'correctly encodes "a" with negative shift' do
-      caesar = CaesarCipher.new
       expect(caesar.encode("a", -5)).to eql("v")
     end
 
     it 'correctly encodes "A" with negative shift' do
-      caesar = CaesarCipher.new
       expect(caesar.encode("A", -5)).to eql("V")
     end
 
     it 'correctly encodes multiple words' do
-      caesar = CaesarCipher.new
       expect(caesar.encode("What a string", 5)).to eql("Bmfy f xywnsl")
     end
 
     it 'correctly encodes special characters' do
-      caesar = CaesarCipher.new
       expect(caesar.encode("!", 5)).to eql("!")
+    end
+
+    it 'correctly encodes large positive shift' do
+      expect(caesar.encode("Aa", 20)).to eql("Uu")
+    end
+
+    it 'correctly encodes large negative shift' do
+      expect(caesar.encode("Zz", -20)).to eql("Ff")
     end
   end
 end
