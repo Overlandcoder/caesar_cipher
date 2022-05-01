c7a22b8c6efcd6b34e1e533569f9d0f5964b8587
Overlander <anuragnandal@hotmail.com>
2022-05-01 18:37:24 -0400

    Remove first 2 tests, add more tests, and finish all testing

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -2,16 +2,6 @@ require './lib/caesar_cipher.rb'
 
 describe CaesarCipher do
   describe "encode" do
-    it 'correctly encodes "a" with positive shift' do
-      caesar = CaesarCipher.new
-      expect(caesar.encode("a", 5)).to eql("f")
-    end
-
-    it 'correctly encodes "A" with positive shift' do
-      caesar = CaesarCipher.new
-      expect(caesar.encode("A", 5)).to eql("F")
-    end
-
     it 'correctly encodes "z" with positive shift' do
       caesar = CaesarCipher.new
       expect(caesar.encode("z", 5)).to eql("e")
@@ -20,6 +10,26 @@ describe CaesarCipher do
     it 'correctly encodes "Z" with positive shift' do
       caesar = CaesarCipher.new
       expect(caesar.encode("Z", 5)).to eql("E")
+    end
+
+    it 'correctly encodes "a" with negative shift' do
+      caesar = CaesarCipher.new
+      expect(caesar.encode("a", -5)).to eql("v")
+    end
+
+    it 'correctly encodes "A" with negative shift' do
+      caesar = CaesarCipher.new
+      expect(caesar.encode("A", -5)).to eql("V")
+    end
+
+    it 'correctly encodes multiple words' do
+      caesar = CaesarCipher.new
+      expect(caesar.encode("What a string", 5)).to eql("Bmfy f xywnsl")
+    end
+
+    it 'correctly encodes special characters' do
+      caesar = CaesarCipher.new
+      expect(caesar.encode("!", 5)).to eql("!")
     end
   end
 end
