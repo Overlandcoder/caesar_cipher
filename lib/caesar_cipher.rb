24e0b7663c692c60f39a51baa79d59739bc2b529
Overlander <anuragnandal@hotmail.com>
2022-05-01 18:49:18 -0400

    Refactor encode method

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,28 +1,38 @@
 class CaesarCipher
+  attr_reader :message
+
   def encode(string, shift)
-    message = ''
+    @message = ''
     string.each_char do |char|
       if char.ord.between?(97, 122)
-        if (char.ord + shift) > 122
-          message += (char.ord - 26 + shift).chr
-        elsif (char.ord + shift) < 97
-          message += (char.ord + 26 + shift).chr
-        else
-          message += (char.ord + shift).chr
-        end
+        encode_noncaps(char, shift) 
       elsif char.ord.between?(65, 90)
-        if (char.ord + shift) > 90
-          message += (char.ord - 26 + shift).chr
-        elsif (char.ord + shift) < 65
-          message += (char.ord + 26 + shift).chr
-        else
-          message += (char.ord + shift).chr
-        end
+        encode_caps(char, shift)
       else
-        message += char
+        @message += char
       end
     end
     message
+  end
+
+  def encode_noncaps(char, shift)
+    if (char.ord + shift) > 122
+      @message += (char.ord - 26 + shift).chr
+    elsif (char.ord + shift) < 97
+      @message += (char.ord + 26 + shift).chr
+    else
+      @message += (char.ord + shift).chr
+    end
+  end
+
+  def encode_caps(char, shift)
+    if (char.ord + shift) > 90
+      @message += (char.ord - 26 + shift).chr
+    elsif (char.ord + shift) < 65
+      @message += (char.ord + 26 + shift).chr
+    else
+      @message += (char.ord + shift).chr
+    end
   end
 end
 
