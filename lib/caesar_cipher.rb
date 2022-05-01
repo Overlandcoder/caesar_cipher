88daa918e9ba07708d386fa3a37ceb18efdc4b02
Overlander <anuragnandal@hotmail.com>
2022-05-01 13:51:32 -0400

    Create a class, and rename variables

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,23 +1,30 @@
-def caesar_cipher(string, shift)
-  result = ''
-  string.each_char do |char|
-    if char.ord.between?(97, 122)
-      if (char.ord + shift) > 122
-        result += (char.ord - 26 + shift).chr
+class CaesarCipher
+  def encode(string, shift)
+    message = ''
+    string.each_char do |char|
+      if char.ord.between?(97, 122)
+        if (char.ord + shift) > 122
+          message += (char.ord - 26 + shift).chr
+        elsif (char.ord + shift) < 97
+          message += (char.ord + 26 + shift).chr
+        else
+          message += (char.ord + shift).chr
+        end
+      elsif char.ord.between?(65, 90)
+        if (char.ord + shift) > 90
+          message += (char.ord - 26 + shift).chr
+        elsif (char.ord + shift) < 65
+          message += (char.ord + 26 + shift).chr
+        else
+          message += (char.ord + shift).chr
+        end
       else
-        result += (char.ord + shift).chr
+        message += char
       end
-    elsif char.ord.between?(65, 90)
-      if (char.ord + shift) > 90
-        result += (char.ord - 26 + shift).chr
-      else
-        result += (char.ord + shift).chr
-      end
-    else
-      result += char
     end
+    message
   end
-  p result
 end
 
-caesar_cipher("What a string!", 5)
+a = CaesarCipher.new
+puts a.encode("What a string!", -5)
